9bd3b6b20c0f9022b28d2ab8a5df1d3055e49f52
francesco <francesco.gadaleta@gmail.com>
2020-11-21 20:45:52 +0100

    cleanup

--- src/model/torch.rs
+++ src/model/torch.rs
@@ -34,32 +34,6 @@ impl TorchSession  {
 }
 
 
-/// Load torchscript from file and prepare torch runtime
-///
-// pub fn load_torchscript(model_file: &str) -> anyhow::Result<CModule> {
-//     let model = tch::CModule::load(model_file)?;
-//     Ok(model)
-// }
-
-/// Perform forward pass of pre-loaded model
-///
-// pub fn forward(model: CModule, input_data: Tensor) -> anyhow::Result<()>{
-//     // let image = imagenet::load_image_and_resize224(image_file)?;
-
-//     // Apply the forward pass of the model to get the logits.
-//     let output = input_data
-//         .unsqueeze(0)
-//         .apply(&model)
-//         .softmax(-1, tch::Kind::Float);
-
-//     Ok(())
-// }
-
-
-
-
-
-
 
 #[cfg(test)]
 mod tests {
