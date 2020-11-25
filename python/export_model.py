424e45011a0993dc532c50ed2cad8bc1e4d20cf9
francesco <francesco.gadaleta@gmail.com>
2020-11-25 15:54:09 +0100

    cleanup

--- python/export_model.py
+++ python/export_model.py
@@ -1,81 +1,17 @@
-import torch
-from torch import nn
 import torch.utils.model_zoo as model_zoo
 import torch.onnx
 import torch.nn as nn
 import torch.nn.init as init
-
-
-class SuperResolutionNet(nn.Module):
-    def __init__(self, upscale_factor, inplace=False):
-        super(SuperResolutionNet, self).__init__()
-
-        self.relu = nn.ReLU(inplace=inplace)
-        self.conv1 = nn.Conv2d(1, 64, (5, 5), (1, 1), (2, 2))
-        self.conv2 = nn.Conv2d(64, 64, (3, 3), (1, 1), (1, 1))
-        self.conv3 = nn.Conv2d(64, 32, (3, 3), (1, 1), (1, 1))
-        self.conv4 = nn.Conv2d(32, upscale_factor ** 2, (3, 3), (1, 1), (1, 1))
-        self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
-
-        self._initialize_weights()
-
-    def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
-        x = self.relu(self.conv3(x))
-        x = self.pixel_shuffle(self.conv4(x))
-        return x
-
-    def _initialize_weights(self):
-        init.orthogonal_(self.conv1.weight, init.calculate_gain('relu'))
-        init.orthogonal_(self.conv2.weight, init.calculate_gain('relu'))
-        init.orthogonal_(self.conv3.weight, init.calculate_gain('relu'))
-        init.orthogonal_(self.conv4.weight)
-
-# Create the super-resolution model by using the above model definition.
-torch_model = SuperResolutionNet(upscale_factor=3)
-
-# Load pretrained model weights
-model_url = 'https://s3.amazonaws.com/pytorch/test_data/export/superres_epoch100-44c6958e.pth'
-batch_size = 1    # just a random number
-
-# Initialize model with the pretrained weights
-map_location = lambda storage, loc: storage
-if torch.cuda.is_available():
-    map_location = None
-torch_model.load_state_dict(model_zoo.load_url(model_url, map_location=map_location))
-
-# set the model to inference mode
-torch_model.eval()
-
-# Input to the model
-x = torch.randn(batch_size, 1, 224, 224, requires_grad=True)
-torch_out = torch_model(x)
-
-# Export the model
-torch.onnx.export(torch_model,               # model being run
-                  x,                         # model input (or a tuple for multiple inputs)
-                  "model.onnx",   # where to save the model (can be a file or file-like object)
-                  export_params=True,        # store the trained parameter weights inside the model file
-                  opset_version=10,          # the ONNX version to export the model to
-                  do_constant_folding=True,  # whether to execute constant folding for optimization
-                  input_names = ['input'],   # the model's input names
-                  output_names = ['output'], # the model's output names
-                  dynamic_axes={'input' : {0 : 'batch_size'},    # variable lenght axes
-                                'output' : {0 : 'batch_size'}})
-
-
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
-
 import torchvision
 import torchvision.transforms as transforms
 from torchvision.utils import make_grid
 import numpy as np
 from random import sample
 
+# Create fake data
 nsamples = 10000
 train_labels = random.choices([0,1,2,3,4,5,6,7,8,9], k=1)
 train_dataset = np.random.rand(100, 34)
@@ -118,6 +54,7 @@ mean_valid_losses = []
 valid_acc_list = []
 epochs = 15
 
+# Train model
 for epoch in range(epochs):
     model.train()
     train_losses = []
@@ -129,8 +66,6 @@ for epoch in range(epochs):
         loss.backward()
         optimizer.step()
         train_losses.append(loss.item())
-        # if (i * 128) % (128 * 100) == 0:
-        #     print(f'{i * 128} / 50000')
     model.eval()
     correct = 0
     total = 0
@@ -142,27 +77,23 @@ for epoch in range(epochs):
             _, predicted = torch.max(outputs.data, 1)
             correct += (predicted == labels).sum().item()
             total += labels.size(0)
-
     mean_train_losses.append(np.mean(train_losses))
     mean_valid_losses.append(np.mean(valid_losses))
-
     accuracy = 100*correct/total
     valid_acc_list.append(accuracy)
     print('epoch : {}, train loss : {:.4f}, valid loss : {:.4f}, valid acc : {:.2f}%'\
          .format(epoch+1, np.mean(train_losses), np.mean(valid_losses), accuracy))
 
-
-
-# set the model to inference mode
+# set the model to inference mode and export
 model.eval()
 # Input to the model
 x = torch.randn(1, 1, 34, requires_grad=True)
 out = model(x)
 
 # Export the model
-torch.onnx.export(torch_model,               # model being run
+torch.onnx.export(model,               # model being run
                   x,                         # model input (or a tuple for multiple inputs)
-                  "simplemodel.onnx",   # where to save the model (can be a file or file-like object)
+                  "simple_model.onnx",   # where to save the model (can be a file or file-like object)
                   export_params=True,        # store the trained parameter weights inside the model file
                   opset_version=10,          # the ONNX version to export the model to
                   do_constant_folding=True,  # whether to execute constant folding for optimization
